2c465b3e6f72365f477e423df2e4c3ecef12a22b
Leonardo Costa <103146352+leocostarj22@users.noreply.github.com>
2022-06-19 13:58:29 -0300

    Projeto Memory Game - Stranger Things
    Criei este game para minha filha, fã número 1 da série da Netflix. Espero que curtam o projeto e se puderem, add mais infos e experiencias no código.

---
+++ scripts.js
@@ -0,0 +1,66 @@
+const cards = document.querySelectorAll(".card");
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add("flip");
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCards();
+    return;
+  }
+
+  unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  musica3.play();
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+  
+
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    musica2.play();
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  });
+})();
+
+cards.forEach((card) => {
+  card.addEventListener("click", flipCard);
+});
+
+
